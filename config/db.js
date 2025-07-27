437a9efbc33dd05d58e8adcb0c0fbac9d40d2950
umarovali <umarovali.345@gmail.com>
2025-07-27 17:14:00 +0600

    ok

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = () => {
-    mongoose.connect("mongodb://127.0.0.1:27017/home-5")
+    mongoose.connect("mongodb+srv://umarovali345:aEa4fqQuGJlafGi0@cluster0.73mvfpw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         .then(() => console.log("Connect to db"))
         .catch((err) => console.log(err))
 }
